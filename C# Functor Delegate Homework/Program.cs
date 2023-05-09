0fb6b61672bf9d84f358d6466e3bc474fb8d66dd
hasanhttps <hasanabdullazad@gmail.com>
2023-05-10 01:39:40 +0400

    Functor

---
+++ C# Functor Delegate Homework/Program.cs
@@ -0,0 +1,17 @@
+﻿using Funct.models;
+using System.Security.Cryptography.X509Certificates;
+
+namespace Funct {
+    public delegate void Funct(string str);
+    internal class Program {
+        static void Main() {
+            Console.WriteLine("Enter string");
+            var str = Console.ReadLine();
+            MyClass cls = new MyClass();
+            Funct funcDell = cls.Space; // params sadece sizin ora vereceyiniz parametrlerdi burda funcDell-e functionlari verirsiniz
+            funcDell += cls.Reverse;
+            Run run = new Run();
+            run.runFunc(funcDell, str); //cagiranda Space() ve Reverse() functionlari ise dusmelidir
+        }
+    }
+}
---
+++ C# Functor Delegate Homework/models/MyClass.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Funct.models {
+    public class MyClass {
+
+        public void Space(string str) {
+            char myChar = '_';
+            Console.WriteLine(string.Concat(str.Select(c => c.ToString() + myChar.ToString())));
+        }
+        public void Reverse(string str) {
+            Console.WriteLine(str.Reverse().ToArray()); 
+        }
+    }
+}
